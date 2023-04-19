87cea4d06e71724ed607d0eb91ef9060c18a1a54
Krunal Priyadarshi <krunalpriyadarshi99@gmail.com>
2023-04-19 14:51:07 -0400

    adding file

--- RSD/JavaScript/script.js
+++ RSD/JavaScript/script.js
@@ -1,23 +1,27 @@
 const header = document.querySelector("header");
 
-window.addEventListener ("scroll", function(){
-    header.classList.toggle("sticky", window.scrollY >0)
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 0)
 });
 
-let menu= document.querySelector('#menu-icon');
-let navlist= document.querySelector('.navlist');
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
 
-menu.onclick=() =>{
+menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.remove('active');
 }
 
-const sr= ScrollReveal({
+const sr = ScrollReveal({
     distance: '45px',
     duration: 2700,
     reset: true
 })
 
-sr.reveal('.home-text', {delay:350, origin:'left'})
-sr.reveal('.home-img', {delay:350, origin:'right'})
-sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact', {delay:200, origin:'bottom'})
+sr.reveal('.home-text', { delay: 350, origin: 'left' })
+sr.reveal('.home-img', { delay: 350, origin: 'right' })
+sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact', { delay: 200, origin: 'bottom' })
+
+function redirectToHome() {
+    window.location.href = 'Riot Games - Home.html';
+}
